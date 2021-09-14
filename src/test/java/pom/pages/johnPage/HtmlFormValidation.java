dca1a1ede50b1eb36a4903153bd91e09d6e1c20a
Jonhleal90 <jlealg52@gmail.com>
2021-09-14 00:18:16 -0500

    Creación de la page para formulario input Validation Examples, se localizan los elementos web y se crean sus acciones.

---
+++ src/test/java/pom/pages/johnPage/HtmlFormValidation.java
@@ -0,0 +1,41 @@
+package pom.pages.johnPage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import pom.pages.BasePage;
+
+public class HtmlFormValidation extends BasePage {
+    public HtmlFormValidation(WebDriver driver) {
+        super(driver);
+    }
+    @FindBy(how = How.ID,using = "firstname")
+    private WebElement inputFirstName;
+    @FindBy(how = How.ID,using = "surname")
+    private WebElement inputSurname;
+    @FindBy(how = How.ID,using = "age")
+    private WebElement inputAge;
+    @FindBy(how = How.CSS,using = "#country > option:nth-child(38)")
+    private WebElement inputCountry;
+    @FindBy(how = How.ID,using = "notes")
+    private WebElement inputNotas;
+    @FindBy(how = How.XPATH,using = "/html/body/div/div[3]/form/input[4]")
+    private WebElement btnEnviar;
+    @FindBy(how = How.NAME,using = "surnamevalidation")
+    private WebElement errorLabelApellido;
+
+    public void  llenarFormulario(String nombre,String apellido,String edad, String pais, String notas){
+        inputFirstName.sendKeys(nombre);
+        inputSurname.sendKeys(apellido);
+        inputAge.sendKeys(edad);
+        inputCountry.click();
+        selectByText(inputCountry,pais);
+        inputNotas.sendKeys(notas);
+        btnEnviar.click();
+    }
+    public String getErrorApellido(){
+        return errorLabelApellido.getText();
+    }
+
+}
