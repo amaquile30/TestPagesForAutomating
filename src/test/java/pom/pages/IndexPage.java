d8e1b12c4fae86fc6eede2fad39cc93d193c033a
Johnleal90 <54120555+Johnleal90@users.noreply.github.com>
2021-09-20 20:40:12 -0500

    Test john  (#3)
    * Creación de la page para formulario input Validation Examples, se localizan los elementos web y se crean sus acciones.
    * Se ajusta la localización del elemento country.

--- src/test/java/pom/pages/IndexPage.java
+++ src/test/java/pom/pages/IndexPage.java
@@ -13,26 +13,26 @@ public class IndexPage extends BasePage {
 
     @FindBy(how = How.ID, using = "htmlformtest")
     private WebElement menuHtmlForm;
-
     @FindBy(how = How.ID, using = "calculatetest")
     private WebElement menuCalculate;
-
     @FindBy(linkText = "HTML Form Example")
     private WebElement lnkHtmlForm;
-
     @FindBy(css = "h1")
     private WebElement tituloIndex;
+    @FindBy(how = How.ID,using = "inputvalidation")
+    private WebElement linkHtmlFormValidation;
 
 
     public void clicHtmlForm() {
         menuHtmlForm.click();
     }
-
     public void clicCalculator() {
         menuCalculate.click();
     }
-
     public String tituloIndexPage() {
         return tituloIndex.getText();
+    }
+    public void clicFormValidation(){
+        linkHtmlFormValidation.click();
     }
 }
