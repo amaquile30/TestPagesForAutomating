8e3c03494db769bc2c672f9f5563be3eff574c13
amaquile30 <amaquile@gmail.com>
2021-09-28 21:16:38 -0500

    Se crea ejecución de navegador chrome sin cabecera

--- src/test/java/pom/test/TestBase.java
+++ src/test/java/pom/test/TestBase.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import pom.pages.IndexPage;
 
@@ -13,7 +14,8 @@ public class TestBase {
 
     public WebDriver driver;
     //private String browser= "firefox";
-    private String browser = "chrome";
+    //private String browser = "chrome";
+    private String browser = "ninguno";
 
     public IndexPage indexPage;
 
@@ -31,7 +33,12 @@ public class TestBase {
                 driver = new FirefoxDriver();
                 break;
             default:
-                Assert.fail("El navegador no esta como opción");
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("disable-gpu");
+                driver = new ChromeDriver(chromeOptions);
         }
 
         driver.get("https://testpages.herokuapp.com/styled/index.html");
